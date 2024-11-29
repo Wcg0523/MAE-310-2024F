1371b32d3e6f8ee6c8b77cffb997c91ee0defdf0
Wu Chenggang <3500748070@qq.com>
2024-11-29 20:53:57 +0800

    2to5element_test

--- codes/proj-01-heat1d/driver.m
+++ codes/proj-01-heat1d/driver.m
@@ -8,7 +8,7 @@ h = 0.0;           % -u,x = h  at x = 0
 % Setup the mesh
 pp   = 2;              % polynomial degree
 n_en = pp + 1;         % number of element or local nodes
-n_el = 2;              % number of elements
+n_el = 5;              % number of elements
 n_np = n_el * pp + 1;  % number of nodal points
 n_eq = n_np - 1;       % number of equations
 n_int = 10;
